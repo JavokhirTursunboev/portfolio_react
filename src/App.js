6764aae33fba7cf4b3e5cc7710a52a4baefebd4c
Javokhir <jtursunboev@bk.ru>
2023-04-25 14:47:29 +0900

    topbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import React, { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact.jsx";
-import './app.scss'
+import "./app.scss";
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
         <Works />
-        <Testimonials/>
-        <Contact/>
+        <Testimonials />
+        <Contact />
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,30 @@
 import "./topbar.scss";
-
-const Topbar = () => {
+import { Person, Mail } from "@mui/icons-material";
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="intro" className="logo">
-            J^blog
+            Javokhir
           </a>
           <div className="itemContainer">
-            
+            <Person className="icon" />
+            <span>+821021151771</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>hijavokhir@gmail.com</span>
+          </div>
+          <div className="itemContainer"></div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
           </div>
         </div>
-        <div className="right"></div>
       </div>
     </div>
   );
