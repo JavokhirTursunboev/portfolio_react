46614719e3c5d69994a6451f410feeead677a2f2
Javokhir <jtursunboev@bk.ru>
2023-04-26 07:43:52 +0900

    intro is done

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,11 +1,27 @@
-import './intro.scss';
+import "./intro.scss";
 
 const Intro = () => {
-    return (
-        <div className='intro' id='intro'>
-          Intro  
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="men" />
         </div>
-    );
-}
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Javokhir.</h1>
+          <h3>
+            Freelance<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
