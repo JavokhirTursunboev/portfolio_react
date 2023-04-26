ed9ea52fde92b62a8c1477667eabd6fcfcf8980c
Javokhir <jtursunboev@bk.ru>
2023-04-26 19:49:44 +0900

    intro is done i install ityped

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
 import "./intro.scss";
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:50,
+      strings: ["Developer", "Content Creator", "Programmer"],
+    });
+  });
+
   return (
-    <div className="intro" id="intro">
+    <div className="intro" id="home">
       <div className="left">
         <div className="imgContainer">
           <img src="assets/man.png" alt="men" />
@@ -13,7 +26,7 @@ const Intro = () => {
           <h2>Hi There, I'm</h2>
           <h1>Javokhir.</h1>
           <h3>
-            Freelance<span></span>
+           Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
