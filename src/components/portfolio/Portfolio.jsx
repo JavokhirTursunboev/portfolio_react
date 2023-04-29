fa81ab5d68ec5f334afc6f5ae86e53e0b90f3ff0
Javokhir <jtursunboev@bk.ru>
2023-04-29 12:15:28 +0900

    portfolio part is done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,5 +2,42 @@ import React from 'react';
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div id='portfolio'></div>;
+  return (
+    <div id="portfolio" className="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,12 @@
+import './portfolioList.scss'
+import React from 'react';
+
+const PortfolioList = () => {
+    return (
+        <li className='portfolioList'>
+            
+        </li>
+    );
+}
+
+export default PortfolioList;
