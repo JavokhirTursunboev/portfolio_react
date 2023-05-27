45539b2a6b2099a2d19ce7f6477c13278a8a5579
Javokhir <jtursunboev@bk.ru>
2023-05-27 19:29:57 +0900

    done testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -34,7 +34,7 @@ export default function Testimonials() {
       <h1>Testimonals</h1>
       <div className="container">
         {data.map((item) => (
-          <div className="card">
+          <div className={item.featured ? "card featured" : "card"}>
             <div className="top">
               <img src="assets/right-arrow.png" className="left" alt="" />
               <img className="user" src={item.img} alt="" />
